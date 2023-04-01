641d51d86d8e6c4f0f71d555ac7944addc220158
Tyler Simoni <tyler.simoni.8@gmail.com>
2023-04-01 12:03:26 -0400

    Initial navbar created

---
+++ src/utils/constants.ts
@@ -0,0 +1,23 @@
+type NavLink = {
+  id: string;
+  text: string;
+};
+
+export const navLinks: Array<NavLink> = [
+  {
+    id: 'useCases',
+    text: 'Use Cases',
+  },
+  {
+    id: 'features',
+    text: 'Features',
+  },
+  {
+    id: 'pricing',
+    text: 'Pricing',
+  },
+  {
+    id: 'resources',
+    text: 'Resources',
+  },
+];
