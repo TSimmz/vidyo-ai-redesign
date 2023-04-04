61317b1e3717644d55b98f2b228596a41a1db400
Tyler Simoni <tyler.simoni.8@gmail.com>
2023-04-04 11:14:12 -0400

    Updates header to have scroll based styling changes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,27 @@
 import Image from 'next/image';
+import { useEffect, useState } from 'react';
 import { navLinks } from '~/utils/constants';
 
 interface IHeader extends React.PropsWithChildren<any> {}
 
 const Header: React.FC<IHeader> = () => {
+  const [isPageScrolled, setIsPageScrolled] = useState(false);
+  const updatePageScrolledStatus = () => {
+    setIsPageScrolled(window.scrollY >= 80);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', updatePageScrolledStatus);
+  }, []);
+
   return (
-    <header className="fixed z-10 h-20 w-full bg-transparent px-20 py-4">
-      <nav className="flex items-center justify-between">
+    <header
+      className={`fixed z-10 flex w-full items-center ${
+        isPageScrolled
+          ? 'h-16 bg-white/95 drop-shadow-md'
+          : 'h-20 bg-transparent'
+      } transition-all duration-500 ease-in-out`}
+    >
         <div
           id="header-logo"
           className="flex items-center gap-3 text-[26px] font-semibold"
@@ -28,7 +43,11 @@ const Header: React.FC<IHeader> = () => {
           ))}
         </ul>
         <div id="header-cta" className="flex items-center gap-3 font-semibold">
-          <button className="w-28 rounded-3xl bg-white px-4 py-[6px]">
+          <button
+            className={`box-border w-28 rounded-3xl bg-white px-4 py-[6px] transition-all duration-[250ms] ease-in-out ${
+              isPageScrolled ? 'border-2 border-zinc-500' : ''
+            }`}
+          >
             LOGIN
           </button>
           <button className="w-28 rounded-3xl bg-black px-5 py-[6px] text-white">
