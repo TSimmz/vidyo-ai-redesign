34f637a53506bd97605b890fbb568d36cd13cf3a
Tyler Simoni <tyler.simoni.8@gmail.com>
2023-04-05 14:14:13 -0400

    Updates tailwind screen theme to include max-width media queries

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import { type Config } from 'tailwindcss';
+import defaultTheme from 'tailwindcss/defaultTheme';
 
 export default {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
@@ -6,6 +7,14 @@ export default {
     extend: {
       backgroundImage: {
         'hero-pattern': "url('/images/hero.jpg')",
+      },
+      screens: {
+        ...defaultTheme.screens,
+        'sm-max': { max: '639px' },
+        'md-max': { max: '767px' },
+        'lg-max': { max: '1023px' },
+        'xl-max': { max: '1279px' },
+        '2xl-max': { max: '1535px' },
       },
     },
   },
